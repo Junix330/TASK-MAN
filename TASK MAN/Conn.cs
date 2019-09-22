5658c982ac33ea2fc6b4a6688ff94c1e1c2df82f
Junix <Junix@JUNIX-PC>
2019-09-23 00:13:35 +0800

    jx

---
+++ TASK MAN/Conn.cs
@@ -0,0 +1,57 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TASK_MAN
+{
+
+    public class Conn
+    {
+        public string Server { get; set; }
+
+        public string Database { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public bool Test()
+        {
+
+
+            var s = string.Format("Server={0};Database='{3}';Uid='{1}';Pwd='{2}';", Server, Username, Password, Database);
+            var c = new MySqlConnection(s);
+
+            try
+            {
+                c.Open();
+                if (c.State == System.Data.ConnectionState.Open)
+                {
+                    c.Close();
+                    c.Dispose();
+                    return true;
+                }
+
+                else
+                {
+                    c.Dispose();
+                    return false;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                XtraMsg.Error(ex.Message);
+                return false;
+            }
+        }
+
+
+    }
+
+
+}
---
+++ TASK MAN/Utils.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TASK_MAN
+{
+    public static class XtraMsg
+    {
+        public static void Show(string msg, string title = "")
+        {
+            MessageBox.Show(msg, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public static void Warn(string msg, string title = "")
+        {
+            MessageBox.Show(msg, title, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        public static void Error(string msg, string title = "")
+        {
+            MessageBox.Show(msg, title, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+
+        public static void Info(string msg, string title = "")
+        {
+            MessageBox.Show(msg, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+    }
+
+
+    public class Utils
+    {
+
+        public static void CfgSet(string key, string val)
+        {
+
+        }
+
+        public static string CfgGet(string key)
+        {
+
+            return "";
+        }
+
+        public static string Clean(object obj)
+        {
+            if (obj == null) return "";
+            var s = "";
+
+            try
+            {
+                s = obj.ToString();
+                s = s.Trim();
+                return s;
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+
+
+    }
+
+}
