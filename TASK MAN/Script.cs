e2bdbbb47a92a2c637ad3637323205246f37c3a2
Junix <Junix@JUNIX-PC>
2019-09-23 00:21:41 +0800

    jx

--- TASK MAN/Script.cs
+++ TASK MAN/Script.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +10,50 @@ namespace TASK_MAN
 {
     public class Script
     {
+        Conn con { get; set; }
+        MySqlConnection sqlCon;
 
+
+        public Script(Conn Connection)
+        {
+            this.con = Connection;
+
+            var s = string.Format("Data Source={0}; User ID='{1}'; Password='{2}'; Initial Catalog='{3}';", con.Server, con.Username, con.Password, con.Database);
+            sqlCon = new MySqlConnection(s);
+
+        }
+
+
+        public bool Run(string q)
+        {
+            try
+            {
+                if (sqlCon.State != ConnectionState.Open) sqlCon.Open();
+
+
+                var cmd = new MySqlCommand(q, sqlCon);
+                cmd.ExecuteNonQuery();
+                sqlCon.Close();
+                cmd.Dispose();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                XtraMsg.Error(ex.Message);
+                return false;
+            }
+        }
+
+        public DataTable Run(string q, string type)
+        {
+            var dt = new DataTable();
+            var da = new MySqlDataAdapter(q, sqlCon);
+            da.Fill(dt);
+            da.Dispose();
+            sqlCon.Close();
+
+            return dt;
+        }
 
 
     }
